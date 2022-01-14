89de00405912a36bbf461489d6287a4da0a85634
Nick146Br <nicol.gomes146@gmail.com>
2022-01-14 15:44:23 -0300

    Module 5

--- Web-Development/Week4/js/arrays.js
+++ Web-Development/Week4/js/arrays.js
@@ -1,15 +1,15 @@
-var array = new Array();
-array[0] = "Yaakov";
-array[1] = 2;
-array[2] = function(name){
-    console.log("Hello " + name);
-}
-array[3] = {
-    course: " HTML, CSS & JS"
-};
-console.log(array);
+// var array = new Array();
+// array[0] = "Yaakov";
+// array[1] = 2;
+// array[2] = function(name){
+//     console.log("Hello " + name);
+// }
+// array[3] = {
+//     course: " HTML, CSS & JS"
+// };
+// console.log(array);
 
-array[2](array[0]);
+// array[2](array[0]);
 
 //short hand array creation
 
@@ -19,6 +19,15 @@ var names = [
     "ellie"
 ]
 
-for(var i = 0; i < names.length; i++){
-    console.log("Hello " + names[i]);
+for(var name in names){
+    console.log("Hello " + names[name]);
+}
+
+var myObj = {
+    name: "Yaakov",
+    course: " HTML, CSS & JS"
+};
+
+for(var prop in myObj) {
+    console.log(prop /*name of the prop*/+ ": " + myObj[prop] /* access the value of prop */);
 }
---
+++ Web-Development/Week5/document_js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (event){
+    function sayHello (event){
+        this.textContent = "Said it!";
+        var name = document.getElementById("name").value;
+        var message = "<h2>Hello " + name + "!</h2>";
+        // document.getElementById("content").textContent = message;
+        document.getElementById("content").innerHTML = message;
+        var title = "Lecture53";
+        if(name === "student"){
+            title = document.querySelector("#title").textContent;
+            title += " with a little funny";
+        }
+        document.querySelector("#title").textContent = title; 
+    }
+    
+    document.querySelector("button").addEventListener("click", sayHello);
+
+    document.querySelector("body").addEventListener("mousemove", 
+    function(event){
+        console.log(event.clientX + " " + event.clientY);
+    })
+})
